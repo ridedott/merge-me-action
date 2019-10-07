6d83391d1b410503e1ae3c6969deaf8f7449ebd7
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2019-10-07 11:52:04 +0200

    refactor: fix for review comments

--- test/TestEnv.js
+++ test/TestEnv.js
@@ -1,13 +1,14 @@
+const { env } = require('process');
 const NodeEnvironment = require('jest-environment-node');
 
-process.env.GITHUB_EVENT_PATH = './test/test.github.payload.json';
-process.env.GITHUB_EVENT_NAME = 'pull_request';
-process.env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
-process.env.GITHUB_REF = 'refs/heads/test-branch';
-process.env.GITHUB_WORKFLOW = 'pull_requests';
-process.env.GITHUB_ACTION = 'test-value';
-process.env.GITHUB_ACTOR = 'test-actor';
-process.env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
+env.GITHUB_EVENT_PATH = './test/test.github.payload.json';
+env.GITHUB_EVENT_NAME = 'pull_request';
+env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
+env.GITHUB_REF = 'refs/heads/test-branch';
+env.GITHUB_WORKFLOW = 'pull_requests';
+env.GITHUB_ACTION = 'test-value';
+env.GITHUB_ACTOR = 'test-actor';
+env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
 
 class CustomEnvironment extends NodeEnvironment {
   constructor(config, context) {
