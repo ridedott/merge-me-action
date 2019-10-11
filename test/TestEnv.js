ce717850b63929664a80de56a8a2534581bba537
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2019-10-11 16:42:33 +0200

    test: add custom context configuration per webhook event trigger

--- test/TestEnv.js
+++ test/TestEnv.js
@@ -1,17 +1,52 @@
 const { env } = require('process');
 const NodeEnvironment = require('jest-environment-node');
 
-env.GITHUB_EVENT_PATH = './test/test.github.payload.json';
-env.GITHUB_EVENT_NAME = 'pull_request';
-env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
-env.GITHUB_REF = 'refs/heads/test-branch';
-env.GITHUB_WORKFLOW = 'pull_requests';
-env.GITHUB_ACTION = 'test-value';
-env.GITHUB_ACTOR = 'test-actor';
-env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
 
 class CustomEnvironment extends NodeEnvironment {
   constructor(config, context) {
+    switch (context.docblockPragmas['webhook-pragma']) {
+      case 'check_suite':
+        env.GITHUB_EVENT_PATH = './test/ctx.check-suite.json';
+        env.GITHUB_EVENT_NAME = 'check_suite';
+        env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
+        env.GITHUB_REF = 'refs/heads/test-branch';
+        env.GITHUB_WORKFLOW = 'Auto merge';
+        env.GITHUB_ACTION = 'ridedottauto-merge-action';
+        env.GITHUB_ACTOR = 'dependabot-preview[bot]';
+        env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
+        break;
+      case 'pull_request':
+        env.GITHUB_EVENT_PATH = './test/ctx.pull-request.json';
+        env.GITHUB_EVENT_NAME = 'pull_request';
+        env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
+        env.GITHUB_REF = 'refs/heads/test-branch';
+        env.GITHUB_WORKFLOW = 'Auto merge';
+        env.GITHUB_ACTION = 'ridedottauto-merge-action';
+        env.GITHUB_ACTOR = 'dependabot-preview[bot]';
+        env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
+        break;
+      case 'push':
+        env.GITHUB_EVENT_PATH = './test/ctx.push.json';
+        env.GITHUB_EVENT_NAME = 'push';
+        env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
+        env.GITHUB_REF = 'refs/heads/test-branch';
+        env.GITHUB_WORKFLOW = 'Continuous Integration';
+        env.GITHUB_ACTION = 'ridedottauto-merge-action';
+        env.GITHUB_ACTOR = 'dependabot-preview[bot]';
+        env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
+        break;
+
+      default:
+        env.GITHUB_EVENT_PATH = './test/test.github.payload.json';
+        env.GITHUB_EVENT_NAME = 'pull_request';
+        env.GITHUB_SHA = 'ffac537e6cbbf934b08745a378932722df287a53';
+        env.GITHUB_REF = 'refs/heads/test-branch';
+        env.GITHUB_WORKFLOW = 'pull_requests';
+        env.GITHUB_ACTION = 'test-value';
+        env.GITHUB_ACTOR = 'test-actor';
+        env.GITHUB_REPOSITORY = 'test-actor/Test-Repo';
+        break;
+    }
     super(config, context);
     this.testPath = context.testPath;
   }
@@ -23,7 +58,7 @@ class CustomEnvironment extends NodeEnvironment {
   async teardown() {
     await super.teardown();
   }
-
+  x
   runScript(script) {
     return super.runScript(script);
   }
