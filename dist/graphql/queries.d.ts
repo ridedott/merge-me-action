b1f94f03d1858bbe2aa15635a316889fc89d2076
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2019-10-14 15:38:33 +0200

    chore: add compliled files

--- dist/graphql/queries.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const findPullRequestNodeIdByPullRequestNumber = "\n  query FindPullRequestNodeId($repositoryOwner: String!, $repositoryName: String!, $pullRequestNumber: Int!) {\n    repository(owner: $repositoryOwner, name: $repositoryName) {\n      pullRequest(number: $pullRequestNumber) {\n        id\n      }\n    }\n  }\n";
-export declare const findPullRequestNodeIdByHeadReferenceName = "\n  query FindPullRequestNodeId($repositoryOwner: String!, $repositoryName: String!, $referenceName: String!) {\n    repository(owner: $repositoryOwner, name: $repositoryName) {\n      pullRequests(headRefName: $referenceName, first: 1) {\n        nodes {\n          id\n        }\n      }\n    }\n  }\n";
-//# sourceMappingURL=queries.d.ts.map
--- dist/src/constants.d.ts
+++ dist/src/constants.d.ts
@@ -1,1 +1,2 @@
 export declare const DEPENDABOT_GITHUB_LOGIN = "dependabot-preview[bot]";
+//# sourceMappingURL=constants.d.ts.map
