3be7c579b33155cbdb3c2ea9048fbd9419636ee8
Natan Sągol <m@merlinnot.com>
2020-01-22 19:48:32 +0100

    fix: correctly fetch a message headline when triggered by a check suite

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -58,6 +58,7 @@ export const findPullRequestInfo = `
           edges {
             node {
               commit {
+                messageHeadline
                 message
               }
             }
