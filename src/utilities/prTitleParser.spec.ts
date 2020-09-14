6d539141e8ed6b9181d6672e71c2e1552ca3adfc
Dunya Kirkali <dunyakirkali@gmail.com>
2020-09-14 08:20:47 +0200

    feat: add basic pr title parser

---
+++ src/utilities/prTitleParser.spec.ts
@@ -0,0 +1,65 @@
+import { parsePRTitle } from './prTitleParser';
+
+var title: string = "";
+
+describe('parsePRTitle', (): void => {
+  describe('given containing MAJOR bump', (): void => {
+    beforeEach((): void => {
+      title = "bump @types/jest from 26.0.12 to 27.0.13"
+    });
+
+    it.each(['MAJOR'])(
+      'returns true',
+      (mergeCategory: string): void => {
+        expect.assertions(1);
+
+        expect(
+          parsePRTitle(title, mergeCategory)
+        ).toStrictEqual(true);
+      },
+    );
+
+    it.each(['MINOR', 'PATCH'])(
+      'returns false',
+      (mergeCategory: string): void => {
+        expect.assertions(1);
+
+        expect(
+          parsePRTitle(title, mergeCategory)
+        ).toStrictEqual(false);
+      },
+    );
+  });
+
+  describe('given title containing MINOR bump', (): void => {
+    beforeEach((): void => {
+      title = "bump @types/jest from 26.0.12 to 26.1.0"
+    });
+
+    it.each(['MAJOR', 'MINOR'])(
+      'returns true',
+      (mergeCategory: string): void => {
+        expect.assertions(1);
+
+        expect(
+          parsePRTitle(title, mergeCategory)
+        ).toStrictEqual(true);
+      },
+    );
+
+    it.each(['PATCH'])(
+      'returns false',
+      (mergeCategory: string): void => {
+        expect.assertions(1);
+
+        expect(
+          parsePRTitle(title, mergeCategory)
+        ).toStrictEqual(false);
+      },
+    );
+  });
+
+  describe('given title containing PATCH bump', (): void => {
+
+  });
+});
---
+++ src/utilities/prTitleParser.ts
@@ -0,0 +1,39 @@
+// TODO: (dunyakirkali) Better naming
+export const parsePRTitle = (
+  title: string,
+  category: string,
+): boolean => {
+
+  if(category === 'MAJOR') {
+    return true;
+  }
+
+  const semVerTitleRegExp = /bump .* from (?<from>.*) to (?<to>.*)/u;
+  const match = semVerTitleRegExp.exec(title);
+
+  if (match) {
+    const semVerRegExp = /^(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)$/u;
+
+    // TODO: (dunyakirkali) Handle optionals
+    const from = match.groups?.from!;
+    const to = match.groups?.to!;
+
+    // TODO: (dunyakirkali) Handle optionals
+    const fromMatch = semVerRegExp.exec(from)!;
+    const toMatch = semVerRegExp.exec(to)!;
+
+    // TODO: (dunyakirkali) Handle optionals
+    const fromVersion = fromMatch.groups!;
+    const toVersion = toMatch.groups!;
+
+    if(parseInt(fromVersion.major) !== parseInt(toVersion.major)) {
+      return false;
+    }
+
+    if(parseInt(fromVersion.minor) !== parseInt(toVersion.minor)) {
+      return category === 'MINOR'
+    }
+  }
+
+  return false;
+};
