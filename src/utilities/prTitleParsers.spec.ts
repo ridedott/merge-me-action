cc650ebe6e7486c49657c15a0239c5c0b8c3c171
Cole Kettler <me@colekettler.com>
2020-12-02 06:17:42 -0500

    fix: make PR title semver regex case-insensitive

--- src/utilities/prTitleParsers.spec.ts
+++ src/utilities/prTitleParsers.spec.ts
@@ -86,4 +86,19 @@ describe('checkPullRequestTitleForMergePreset', (): void => {
       },
     );
   });
+
+  describe('given title is capitalized', (): void => {
+    const title = 'Bump @types/jest from 26.0.12 to 27.0.13';
+
+    it.each(['DEPENDABOT_MINOR', 'DEPENDABOT_PATCH'])(
+      'returns false',
+      (mergeCategory: string): void => {
+        expect.assertions(1);
+
+        expect(
+          checkPullRequestTitleForMergePreset(title, mergeCategory),
+        ).toStrictEqual(false);
+      },
+    );
+  });
 });
