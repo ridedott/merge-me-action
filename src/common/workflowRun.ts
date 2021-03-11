8b0f5e6d6db22b18c205958f193ba6580a27c382
Petr Rusanov <petr@ridedott.com>
2021-03-11 16:11:22 +0100

    fix: fix

--- src/common/workflowRun.ts
+++ src/common/workflowRun.ts
@@ -30,7 +30,7 @@ export const getLastWorkflowRunConclusion = async (
     branch: query.branch,
     owner: query.owner,
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    per_page: 1,
+    per_page: 100,
     repo: query.repository,
     status: 'completed',
     // eslint-disable-next-line @typescript-eslint/naming-convention
