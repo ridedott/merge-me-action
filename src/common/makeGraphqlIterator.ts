085d4156d5697e94fc9d095bc27186590c98bc7c
Natan Sągol <m@merlinnot.com>
2021-04-12 11:25:14 +0200

    simplify the generator

--- src/common/makeGraphqlIterator.ts
+++ src/common/makeGraphqlIterator.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+/* eslint-disable no-await-in-loop */
 /* eslint-disable immutable/no-let */
 /* eslint-disable prefer-destructuring */
-/* eslint-disable require-atomic-updates */
 
 import { getOctokit } from '@actions/github';
 import { GraphQlQueryResponseData } from '@octokit/graphql';
@@ -28,55 +27,27 @@ export async function* makeGraphqlIterator<IterableData>(
   ) => IterableList<IterableData> | undefined,
   pageSize: number = MAX_PAGE_SIZE,
 ): AsyncGenerator<IterableData> {
-  const items: IterableData[] = [];
-  let current = 0;
   let cursor: string | undefined = undefined;
   let hasNextPage: boolean = true;
 
-  const fetchPage = async (): Promise<IterableData[] | undefined> => {
-    const response = await octokit.graphql<GraphQlQueryResponseData | null>(
-      query,
-      {
-        ...parameters,
-        endCursor: cursor,
-        pageSize,
-      },
-    );
-
-    if (response === null) {
-      return undefined;
-    }
+  while (hasNextPage) {
+    const response = await octokit.graphql<GraphQlQueryResponseData>(query, {
+      ...parameters,
+      endCursor: cursor,
+      pageSize,
+    });
 
     const list = extractListFunction(response);
 
     if (list === undefined) {
-      return undefined;
+      return;
     }
 
-    const { edges, pageInfo } = list;
+    cursor = list.pageInfo.endCursor;
+    hasNextPage = list.pageInfo.hasNextPage;
 
-    cursor = pageInfo.endCursor;
-    hasNextPage = pageInfo.hasNextPage;
-
-    return edges.map((edge): IterableData => edge.node);
-  };
-
-  /* eslint-disable-next-line no-unmodified-loop-condition */
-  while (current < items.length || hasNextPage) {
-    if (current + 1 >= items.length && hasNextPage) {
-      /* eslint-disable-next-line no-await-in-loop */
-      const data = await fetchPage();
-
-      if (data === undefined) {
-        return;
-      }
-
-      items.push(...data);
-    }
-
-    if (current < items.length) {
-      yield items[current];
-      current++;
+    for (const { node } of list.edges) {
+      yield node;
     }
   }
 }
