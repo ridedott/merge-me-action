f469361c803a9930b592c155cb898ed26a1e0b62
Colin Broderick <colin@cbroderick.me>
2021-10-18 11:33:48 +0200

    chore: fix types error

--- src/utilities/log.spec.ts
+++ src/utilities/log.spec.ts
@@ -15,7 +15,7 @@ const errorWithStack = new Error('I am an error.');
 errorWithStack.stack = 'I am a stack.';
 
 describe.each<
-  [string, (value: unknown) => void, jest.SpyInstance<void, [Error | string]>]
+  [string, (value: unknown) => void, jest.SpyInstance<void, [message: Error | string, properties?: core.AnnotationProperties]>]
 >([
   ['logError', logError, errorSpy],
   ['logWarning', logWarning, warningSpy],
@@ -24,7 +24,7 @@ describe.each<
   (
     _: string,
     logFunction: (value: unknown) => void,
-    coreFunction: jest.SpyInstance<void, [Error | string]>,
+    coreFunction: jest.SpyInstance<void, [message: Error | string, properties?: core.AnnotationProperties]>,
   ): void => {
     it.each<[unknown, string]>([
       ['I am a string.', 'I am a string.'],
